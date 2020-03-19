38d2a9883b4f626101757ab02fd002b2af8aaabf
Travis J Dean <travis.j.dean@gmail.com>
2020-03-18 23:58:49 -0400

    Merge branch 'master'

--- wled00/wled03_set.ino
+++ wled00/wled03_set.ino
@@ -138,6 +138,7 @@ void handleSettingsSet(AsyncWebServerRequest *request, byte subPage)
     notifyTwice = request->hasArg("S2");
 
     receiveDirect = request->hasArg("RD");
+    e131SkipOutOfSequence = request->hasArg("ES");
     e131Multicast = request->hasArg("EM");
     t = request->arg("EU").toInt();
     if (t > 0  && t <= 63999) e131Universe = t;
@@ -372,6 +373,13 @@ bool handleSet(AsyncWebServerRequest *request, const String& req)
   DEBUG_PRINT("API req: ");
   DEBUG_PRINTLN(req);
 
+  //write presets and macros saved to flash directly?
+  bool persistSaves = true;
+  pos = req.indexOf("NP");
+  if (pos > 0) {
+    persistSaves = false;
+  }
+
   //save macro, requires &MS=<slot>(<macro>) format
   pos = req.indexOf("&MS=");
   if (pos > 0) {
@@ -381,7 +389,7 @@ bool handleSet(AsyncWebServerRequest *request, const String& req)
       int en = req.indexOf(')');
       String mc = req.substring(pos);
       if (en > 0) mc = req.substring(pos, en);
-      saveMacro(i, mc);
+      saveMacro(i, mc, persistSaves);
     }
 
     pos = req.indexOf("IN");
@@ -461,7 +469,7 @@ bool handleSet(AsyncWebServerRequest *request, const String& req)
   if (pos > 0) presetApplyBri = (req.charAt(pos+3) != '0');
 
   pos = req.indexOf("PS="); //saves current in preset
-  if (pos > 0) savePreset(getNumVal(&req, pos));
+  if (pos > 0) savePreset(getNumVal(&req, pos), persistSaves);
 
   //apply preset
   if (updateVal(&req, "PL=", &presetCycCurr, presetCycleMin, presetCycleMax)) {
